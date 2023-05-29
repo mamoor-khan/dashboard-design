ab1421f1fe2f21cb44577b5503b1c80ed8055680
mamoor <mamoor99@gmail.com>
2023-05-29 21:50:06 +0530

    project upload on gh

---
+++ src/components/List.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function List({ list }) {
+  return (
+    <ul className='list-container'>
+      {list.map((item) => (
+        <li key={item.label} className={item.active ? 'active' : ''}>
+          <img width='22px' src={`assets/${item.imgPath}`} alt='' />
+          <label>{item.label}</label>
+          <span className='next-icon'>
+            <img src='assets/next.svg' width='16px' alt='' />
+          </span>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default List
